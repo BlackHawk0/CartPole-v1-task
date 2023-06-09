ccacb5a38867b1b3b44f821003e7ea4f7e665c3c
Daniel Ngoya <danielngoys@outlook.com>
2023-06-09 09:13:34 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# import  libraries
+import gymnasium as gym
+import  math
+import random
+
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import  count
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+# if GPU is to be used
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
